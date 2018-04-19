d29762aeeda6151528b50023bfac84cea9dd889a
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-19 15:57:21 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,11 +22,11 @@ end
 def monopoly_with_second_tier
   monopoly = {
 	  :railroads => {
-	    :pieces=>4,
-	    :name => {}
-	    
-	  }
-	}
+	    :pieces=>4
+	   },
+	  :name => {}
+    
+  }
 
 
 	
