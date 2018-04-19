14bea69dd9284d2de2d808b7ecc0c7c6b92eeafe
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-19 15:36:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,30 +1,52 @@
 def new_hash
+  new = {} 
+  empty = {}
 
 end
 
 def actor
-
-end
+  famous_meat_head = {name:"Dwayne The Rock Johnson"} 
+  
+  end
 
 def monopoly
-	monopoly = {}
-
+	monopoly = {
+	  :railroads => {
+	    
+	    
+	    
+	  }
+	 }
 end
 
 def monopoly_with_second_tier
+  	monopoly = {
+	  :railroads => {
+	    :pieces=>4, 
+	    :name => {}
+	    
+	  }
+	 }
+
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	
 end
 
+
 def monopoly_with_third_tier
+  
+	
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 		
 end
 
+
+
+
 def monopoly_with_fourth_tier
+  
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	
-end
+	end
 
 
 
